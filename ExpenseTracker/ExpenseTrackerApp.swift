798768e7cc5c3a72a773a8f4ba0848cefda39744
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-03-26 21:02:27 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Sabyrzhan Tynybayev on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
